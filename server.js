864fa110e23e16770fdc98db5ea8ccfd6488cf18
Dylan Ambrose <djambro44@gmail.com>
2020-06-15 19:19:32 -0700

    updating db with new scraped data

--- server.js
+++ server.js
@@ -16,23 +16,24 @@ app.use(express.json());
 
 app.get("/scrape", function (req, res) {
 
-    axios.get("http://www.echojs.com/").then(function (response) {
+    axios.get("https://www.cbssports.com/fantasy/football/").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
+        // console.log($);
 
         // Now, we grab every h2 within an article tag, and do the following:
-        $("article h2").each(function (i, element) {
+        $("h4 > a").each(function (i, element) {
             // Save an empty result object
             var result = {};
-
+          // console.log("this is what we need! " + $(this).text());
             // Add the text and href of every link, and save them as properties of the result object
             result.title = $(this)
-                .children("a")
+                // .children("a")
                 .text();
             result.link = $(this)
-                .children("a")
+                // .children("a")
                 .attr("href");
-
+          console.log("this is the result" + JSON.stringify(result));
             // Create a new Article using the `result` object built from scraping
             db.Article.create(result)
                 .then(function (dbArticle) {
@@ -47,6 +48,7 @@ app.get("/scrape", function (req, res) {
 
         // Send a message to the client
         res.send("Scrape Complete");
+     
 
     })
 });
@@ -58,8 +60,8 @@ app.get("/scrape", function (req, res) {
 // });
 
 app.get("/", function (req, res){
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-})
+  res.render("index", objectFromDB);
+});
 
 // app.listen(PORT, function() {
 //     console.log("App running on port " + PORT + "!");
